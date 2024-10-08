825058fece6e12d05b9f2f79316ec99551ac7f7f
Jack Greiner <jack@emoss.org>
2024-10-08 14:15:17 -0400

    wrapper.c: Attempt a new way of assembling the command to avoid issues with quotes.

--- src/wrapper.c
+++ src/wrapper.c
@@ -2,18 +2,42 @@
 
 #include <windows.h>
 #include <stdio.h>
-#include "shlwapi.h"
+#include <shlwapi.h>
 
-// Function to check if an option is a single or last option
-static inline BOOL is_single_or_last_option(WCHAR *opt) {
-    return ((!_wcsnicmp(opt, L"-c", 2) && _wcsnicmp(opt, L"-config", 7)) || !_wcsnicmp(opt, L"-n", 2) ||
-            !_wcsnicmp(opt, L"-f", 2) || !wcscmp(opt, L"-") || !_wcsnicmp(opt, L"-enc", 4) ||
-            !_wcsnicmp(opt, L"-m", 2) || !_wcsnicmp(opt, L"-s", 2));
+// Function to get the arguments part of the command line, skipping the executable name
+LPCWSTR SkipProgramName(LPCWSTR cmdline) {
+    LPCWSTR p = cmdline;
+
+    // Skip leading whitespace
+    while (*p && iswspace(*p)) p++;
+
+    if (*p == L'"') {
+        p++;
+        while (*p) {
+            if (*p == L'"') {
+                if (*(p + 1) == L'"') {
+                    p += 2;
+                } else {
+                    p++;
+                    break;
+                }
+            } else {
+                p++;
+            }
+        }
+    } else {
+        while (*p && !iswspace(*p)) p++;
+    }
+
+    // Skip whitespace between program name and arguments
+    while (*p && iswspace(*p)) p++;
+
+    return p;
 }
 
-/* 
-Following function taken from https://creativeandcritical.net/downloads/replacebench.c which is in public domain; 
-Credits to the there mentioned authors. replaces in the string "str" all the occurrences of the string "sub" with the string "rep" 
+/*
+Following function taken from https://creativeandcritical.net/downloads/replacebench.c which is in public domain;
+Credits to the there mentioned authors. Replaces in the string "str" all the occurrences of the string "sub" with the string "rep".
 */
 static inline wchar_t *replace_smart(wchar_t *str, wchar_t *sub, wchar_t *rep) {
     size_t slen = wcslen(sub);
@@ -48,14 +72,6 @@ static inline wchar_t *replace_smart(wchar_t *str, wchar_t *sub, wchar_t *rep) {
     return b ? b : buf;
 }
 
-// Function to replace double quotes with single quotes
-void replace_double_with_single_quotes(wchar_t *str) {
-    wchar_t *modified_str = replace_smart(str, L"\"", L"'");
-    if (modified_str) {
-        wcscpy_s(str, wcslen(modified_str) + 1, modified_str);
-        HeapFree(GetProcessHeap(), 0, modified_str);
-    }
-}
 
 // Function to check if the process is running under WOW64 (32-bit process on 64-bit Windows)
 BOOL is_wow64() {
@@ -73,83 +89,46 @@ void get_program_files_path(wchar_t *path, size_t size) {
     }
 }
 
-int wmain(int argc, wchar_t *argv[]) {
-    BOOL read_from_stdin = FALSE;
-    wchar_t cmdlineW[4096] = L"", pwsh_pathW[MAX_PATH] = L"", bufW[MAX_PATH] = L"";
-    DWORD exitcode;
-    STARTUPINFOW si = {0};
-    PROCESS_INFORMATION pi = {0};
-    int i, j;
-
-    // Get the correct Program Files path based on the process architecture
-    get_program_files_path(pwsh_pathW, MAX_PATH);
-
-    // Set environment variable to disable color rendering output 
-    _wputenv_s(L"NO_COLOR", L"1");
-    // or _wputenv_s(L"TERM", L"xterm-mono");
-
-    // Concatenate options into new cmdline, handling some incompatibilities
-    for (i = 1; argv[i] && !wcsncmp(argv[i], L"-", 1); i++) {
-        if (!is_single_or_last_option(argv[i])) i++;
-        if (!argv[i]) break;
-    }
-
-    for (j = 1; j < i; j++) {
-        if (!wcscmp(L"-", argv[j])) {
-            if (j == (argc - 1)) {
-                read_from_stdin = TRUE;
-                continue;
-            } else {
-                fputws(L"Invalid usage\n", stderr);
-                return 1;
-            }
+// Function to determine if '-c' should be added to the command line
+BOOL should_add_c_option(LPCWSTR args) {
+    // Check if the first argument starts with '-' or '/'
+    if (*args == L'-' || *args == L'/') {
+        LPCWSTR opt = args + 1;
+        if (!_wcsnicmp(opt, L"c", 1) || !_wcsnicmp(opt, L"Command", 7) ||
+            !_wcsnicmp(opt, L"enc", 3) || !_wcsnicmp(opt, L"EncodedCommand", 14) ||
+            !_wcsnicmp(opt, L"file", 4) || !_wcsnicmp(opt, L"f", 1)) {
+            return FALSE;
         }
-        if (!_wcsnicmp(argv[j], L"-ve", 3)) {
-            j++;
-            continue;
-        }
-        if (!_wcsnicmp(argv[j], L"-nop", 4)) {
-            continue;
-        }
-        wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), L" ");
-        wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), argv[j]);
     }
+    return TRUE;
+}
 
-    // Insert '-c' if necessary
-    if (argv[i] && _wcsnicmp(argv[i - 1], L"-c", 2) && _wcsnicmp(argv[i - 1], L"-enc", 4) &&
-        _wcsnicmp(argv[i - 1], L"-f", 2) && _wcsnicmp(argv[i], L"/c", 2)) {
-        wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), L" -c ");
+// Function to check if input is coming from a pipeline
+BOOL is_input_from_pipeline() {
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD mode = 0;
+    if (!GetConsoleMode(hStdin, &mode)) {
+        // If GetConsoleMode fails, it means the input is not from a console, so it's from a pipeline
+        return TRUE;
     }
+    return FALSE;
+}
 
-    // Concatenate the rest of the arguments into the new cmdline
-    for (j = i; j < argc; j++) {
-        wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), L" ");
-        wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), argv[j]);
+// Function to escape double quotes in a string
+void escape_quotes(LPCWSTR input, LPWSTR output, size_t output_size) {
+    wchar_t *escaped_str = replace_smart((wchar_t *)input, L"\"", L"\\\"");
+    if (escaped_str) {
+        wcsncpy_s(output, output_size, escaped_str, _TRUNCATE);
+        HeapFree(GetProcessHeap(), 0, escaped_str);
+    } else {
+        // If replace_smart failed, copy the input as-is
+        wcsncpy_s(output, output_size, input, _TRUNCATE);
     }
+}
 
-    // Support pipeline to handle something like "$(get-date) | powershell -"
-    if (read_from_stdin) {
-        WCHAR defline[4096]; // Buffer to store converted line
-        char line[4096];     // Buffer to store input line
-        HANDLE input = GetStdHandle(STD_INPUT_HANDLE); // Get the standard input handle
-        DWORD type = GetFileType(input);               // Get the file type of the input handle
-
-        // Check if input is redirected (e.g., via pipe)
-        if (type != FILE_TYPE_CHAR) { // Not redirected (FILE_TYPE_PIPE or FILE_TYPE_DISK)
-            // Check if the last argument is "-" and the second-to-last argument is not "-c"
-            if (!wcscmp(argv[argc - 1], L"-") && _wcsnicmp(argv[argc - 2], L"-c", 2)) {
-                wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), L" -c "); // Append "-c" to cmdlineW
-            }
-            wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), L" "); // Append a space to cmdlineW
-            // Read input line by line and append to cmdlineW after converting to wide characters
-            while (fgets(line, 4096, stdin) != NULL) {
-                mbstowcs(defline, line, 4096); // Convert input line to wide characters
-                wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), defline);    // Append converted line to cmdlineW
-            }
-        }
-    } // End support pipeline
-
-    // Replace incompatible commands/strings in the cmdline fed to pwsh.exe
+// Function to perform command/string replacements based on environment variables
+void apply_environment_replacements(LPWSTR cmdline) {
+    WCHAR bufW[MAX_PATH];
     if (GetEnvironmentVariableW(L"PSHACKS", bufW, MAX_PATH)) {
         WCHAR buf_fromW[MAX_PATH];
         WCHAR buf_toW[MAX_PATH];
@@ -163,8 +142,8 @@ int wmain(int argc, wchar_t *argv[]) {
             wchar_t *tokenB = wcstok_s(buf_toW, L"¶", &bufferB);
 
             while (tokenA && tokenB) {
-                buf_replacedW = replace_smart(cmdlineW, tokenA, tokenB);
-                wcscpy_s(cmdlineW, wcslen(buf_replacedW) + 1, buf_replacedW);
+                buf_replacedW = replace_smart(cmdline, tokenA, tokenB);
+                wcscpy_s(cmdline, wcslen(buf_replacedW) + 1, buf_replacedW);
                 HeapFree(GetProcessHeap(), 0, buf_replacedW);
 
                 tokenA = wcstok_s(NULL, L"¶", &bufferA);
@@ -172,21 +151,69 @@ int wmain(int argc, wchar_t *argv[]) {
             }
         }
     }
+}
 
-    /*
-    Replace double quotes with single quotes in the cmdline
-    This is for invokations that use double quotes with arguments that have spaces in them
-    This causes issues with pwsh parsing the invokation when you call the wrapper directly
+int wmain() {
+    wchar_t cmdlineW[8192] = L"", pwsh_pathW[MAX_PATH] = L"", pwsh_exeW[MAX_PATH] = L"";
+    DWORD exitcode;
+    STARTUPINFOW si = {0};
+    PROCESS_INFORMATION pi = {0};
 
-    e.g. 
-    powershell.exe Start-Process -Verb RunAs -FilePath "path to EasyAntiCheat_EOS_Setup.exe" -ArgumentList "install ****" 
-    turns into: 
-    pwsh -c Start-Process -Verb RunAs -FilePath 'path to EasyAntiCheat_EOS_Setup.exe' -ArgumentList 'install ****'
-    */
-    replace_double_with_single_quotes(cmdlineW);
+    // Initialize STARTUPINFO structure
+    si.cb = sizeof(STARTUPINFOW);
+
+    // Get the correct Program Files path based on the process architecture
+    get_program_files_path(pwsh_pathW, MAX_PATH);
+
+    // Get the executable name (pwsh.exe)
+    wcscpy_s(pwsh_exeW, MAX_PATH, PathFindFileNameW(pwsh_pathW));
+
+    // Set environment variable to disable color rendering output
+    _wputenv_s(L"NO_COLOR", L"1");
+
+    // Get the full command line
+    LPCWSTR cmdline_full = GetCommandLineW();
+
+    // Skip the program name
+    LPCWSTR argsW = SkipProgramName(cmdline_full);
+
+    // Build the command line to pass to pwsh.exe
+    wcscpy_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), L"\"");
+    wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), pwsh_exeW);
+    wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), L"\"");
+
+    // Check if we need to add '-c' option
+    if (*argsW) {
+        if (should_add_c_option(argsW)) {
+            wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), L" -c");
+
+            // Enclose the command in double quotes
+            wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), L" \"");
+
+            // Escape any double quotes in argsW
+            wchar_t escaped_args[8192];
+            escape_quotes(argsW, escaped_args, sizeof(escaped_args) / sizeof(wchar_t));
+
+            wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), escaped_args);
+            wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), L"\"");
+        } else {
+            // Append the rest of the arguments as is
+            wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), L" ");
+            wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), argsW);
+        }
+    } else if (is_input_from_pipeline()) {
+        // If there's no arguments but input is from pipeline, we need to read from stdin
+        wcscat_s(cmdlineW, sizeof(cmdlineW) / sizeof(wchar_t), L" -c -");
+    }
+
+    // Perform command/string replacements based on environment variables
+    apply_environment_replacements(cmdlineW);
 
     // Execute the command through pwsh.exe
-    CreateProcessW(pwsh_pathW, cmdlineW, 0, 0, 0, 0, 0, 0, &si, &pi);
+    if (!CreateProcessW(pwsh_pathW, cmdlineW, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
+        fwprintf(stderr, L"Failed to create process. Error code: %lu\n", GetLastError());
+        return 1;
+    }
     WaitForSingleObject(pi.hProcess, INFINITE);
     GetExitCodeProcess(pi.hProcess, &exitcode);
     CloseHandle(pi.hProcess);
